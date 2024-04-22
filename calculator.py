f2a15745c99f295f08cc143d51a21a899d26259b
Ferbos Damien <damien.ferbos@univ-pau.fr>
2024-04-22 15:42:26 +0200

    rajout taux ( x 2 =/= ) , taux TVA sur livraison

--- calculator.py
+++ calculator.py
@@ -8,3 +8,56 @@ def rounder(num, up=True):
         return ceil(num * mul)/mul
     else:
         return floor(num*mul)/mul
+
+
+
+def calcul_frais_port(produits, frais_port):
+
+    total_par_taux = {}
+
+    for p in produits:
+        if not p['taux'] in total_par_taux:
+            total_par_taux.update({p['taux']:p['prix_total']})
+        else:
+            ancien_total = total_par_taux[p['taux']]
+            total_par_taux.update({p['taux']:p['prix_total']+ancien_total})
+
+
+    max_value = max(total_par_taux.values())
+
+
+    taux_retenu = 0
+
+    for key, value in total_par_taux.items():
+        if value == max_value:
+            # en cas d'égalité, on prend le taux le plus haut.
+            if taux_retenu == 0:
+                taux_retenu = key
+            elif key > taux_retenu:
+                taux_retenu = key
+
+
+
+    print(f'taux retenu : {taux_retenu}  pour {max_value} parmi les valeurs {total_par_taux}')
+
+    # on recupere les frais de ports avec leatxu retenu
+    frais_port_final = rounder((1 - taux_retenu/100) * frais_port, 2)
+
+    print(f'frais de port final : {frais_port} - {taux_retenu}%  = {(1-taux_retenu/100) * frais_port} arrondi à {frais_port_final}')
+
+    return frais_port_final, taux_retenu
+
+
+def calcul_total_tva_par_taux(produits):
+    total_par_taux = {}
+
+    for p in produits:
+        if not p['taux'] in total_par_taux:
+            total_par_taux.update({p['taux']: p['prix_tva']})
+        else:
+            ancien_total = total_par_taux[p['taux']]
+            total_par_taux.update({p['taux']: p['prix_tva'] + ancien_total})
+
+    return total_par_taux
+
+
--- main.py
+++ main.py
@@ -6,9 +6,7 @@ import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
 
-from math import ceil
-
-from calculator import rounder
+from calculator import rounder, calcul_frais_port, calcul_total_tva_par_taux
 from settings import TAUX, DISCOUNT
 
 
@@ -21,6 +19,7 @@ def is_nan(value):
 
 def create_hashmap_from_csv(csv_file):
     nombre_lignes = 0
+
     df = pd.read_csv(csv_file)
     hashmap = {}
 
@@ -46,11 +45,15 @@ def get_taux_from_sku_pays(sku, pays):
     sku=str(sku)
     if pays in TAUX:
         if sku[0] == '1' or sku[0] == '4':
+            print(f'Pays ={pays} et SKU commence par {sku[0]} => taux = {TAUX[pays][1]}')
             return TAUX[pays][2]
         if sku[0] == '2' or sku[0] == '3':
+            print(f'Pays ={pays} et SKU commence par {sku[0]} => taux = {TAUX[pays][1]}')
             return TAUX[pays][1]
     else :
+        print("Pays hors UE => pas de taux applicable")
         return None
+
 
 
 def get_produit_details(value):
@@ -61,50 +64,66 @@ def get_produit_details(value):
     prix_unitaire = value[4]
     prix_total = value[11]
     monnaie = value[12]
+    prix_tva = 0
 
     sku = value[32]
     pays = value[23]
 
+    print('-------------------------')
+    print("calcul du taux")
     taux = get_taux_from_sku_pays(sku, pays)
 
-    if taux :
-        prix_total_ht = rounder(quantite * prix_unitaire * (1 - taux / 100),2)
-    else:
-        prix_total_ht = rounder(quantite * prix_unitaire,2)
 
-    return  [
-                date_vente,
-                nom_produit,
-                quantite,
-                prix_unitaire,
-                prix_total,
-                prix_total_ht,
-                monnaie,
-                sku
-    ]
+    print('-------------------------')
+    print("calcul du prix total HT")
+    if taux :
+        prix_tva = rounder(prix_total * taux / 100,2)
+        print(f'taxe à payer : prix_total x (taux / 100) => {prix_total} x {taux} /100  = {prix_total *taux / 100 } ')
+        print(f'Arrondi superieur pour la taxe :{prix_tva}')
+        prix_total_ht = rounder(prix_total - prix_tva,2)
+        print(f"Prix total HT = prix_total - taxe à payer => {prix_total} - {prix_tva}  = {prix_total_ht} ")
+    else:
+        print(f"Pas de taux : prix_total_ht = prix_total donc {prix_total}")
+        prix_total_ht = prix_total
+
+    return  {
+        'date_vente' : date_vente,
+        'nom_produit' : nom_produit ,
+        'quantite' : quantite,
+        'prix_unitaire' : prix_unitaire,
+        'prix_total' : prix_total,
+        'prix_total_ht' : prix_total_ht,
+        'monnaie' : monnaie,
+        'taux' : taux,
+        'sku' : sku,
+        'prix_tva': prix_tva
+    }
 
 
 def generate_pdf_from_csv(csv_file, html_template, output_dir):
 
+    # compteur de factures générées
     total_factures_generees = 0
-    # Lecture du fichier CSV
-    df = pd.read_csv(csv_file)
 
-    # Chargement du template HTML
-    env = Environment(loader=FileSystemLoader('.'),
-                      comment_start_string='{=',
-                      comment_end_string='=}',
-                      )
-    template = env.get_template(html_template)
+    # Préparation de l'environnement + recupération template
+    template = get_template(html_template, output_dir)
 
-    # Création du répertoire de sortie s'il n'existe pas déjà
-    os.makedirs(output_dir, exist_ok=True)
-
+    # Tri des données du CSV
     hashmap = create_hashmap_from_csv(csv_file)
 
     # Afficher la hashmap résultante
     for key, value in hashmap.items():
 
+        print('')
+        print(f'COMMANDE {key}')
+
+        taux = None
+        # Déduction si commande soumise à TVA
+        # pays de commande
+        pays_commande = value[0][23]
+        tva_applicable = pays_commande in TAUX
+        if tva_applicable:
+            taux = get_taux_from_sku_pays(value[0][32], pays_commande)
         frais_port = 0
 
         produits = []
@@ -118,30 +137,62 @@ def generate_pdf_from_csv(csv_file, html_template, output_dir):
 
             produit = get_produit_details(value[x])
             produits.append(produit)
-            total_produits_ht += produit[5]
+            total_produits_ht += produit['prix_total_ht']
+            total_produits_ttc += produit['prix_total']
+            total_produits_tva += produit['prix_tva']
             frais_port += value[x][9]
+            total_discount += value[x][7]
 
-        tva_applicable = value[0][23] in TAUX
-        total_produits_ht = round(total_produits_ht,2)
 
-        taux = None
+        total_produits_ht = rounder(total_produits_ht,2)
 
-        if tva_applicable:
-            taux = get_taux_from_sku_pays(value[0][32], value[0][23])
-            total_produits_tva = round(total_produits_ht*taux/100,2)
-            total_produits_ttc = total_produits_ht + total_produits_tva
-        else :
-            total_produits_tva = total_produits_ht
-            total_produits_ttc = total_produits_ht
+        # frais de port conditionnés à la TVA
+        print('-------------------------')
+        print("calcul des frais de port")
+        if tva_applicable :
+            frais_port_final, taux_retenu = calcul_frais_port(produits, frais_port)
+            total_par_taux = calcul_total_tva_par_taux(produits)
+        else:
+            frais_port_final = frais_port
+            total_par_taux = None
+
+        if total_par_taux and len(total_par_taux) == 1:
+            total_produits_tva1 = rounder(list(total_par_taux.values())[0] + frais_port - frais_port_final, 2)
+            taux1 = list(total_par_taux.keys())[0]
+            total_produits_tva2 = None
+            taux2 = None
+        elif total_par_taux and len(total_par_taux) == 2:
+            if list(total_par_taux.values())[0] == taux_retenu:
+                total_produits_tva1 = rounder(list(total_par_taux.values())[0]+ frais_port - frais_port_final, 2)
+                taux1 = list(total_par_taux.keys())[0]
+                total_produits_tva2 = rounder(list(total_par_taux.values())[1], 2)
+                taux2 = list(total_par_taux.keys())[1]
+            else:
+                total_produits_tva1 = rounder(list(total_par_taux.values())[0], 2)
+                taux1 = list(total_par_taux.keys())[0]
+                total_produits_tva2 = rounder(list(total_par_taux.values())[1] + frais_port - frais_port_final, 2)
+                taux2 = list(total_par_taux.keys())[1]
+        else:
+            total_produits_tva1 = None
+            taux1 = None
+            total_produits_tva2 = None
+            taux2 = None
+
+        # if tva_applicable:
+        #     taux = get_taux_from_sku_pays(value[0][32], pays_commande)
+        #     total_produits_tva = round(total_produits_ht*taux/100,2)
+        #     total_produits_ttc = total_produits_ht + total_produits_tva
+        # else :
+        #     total_produits_tva = total_produits_ht
+        #     total_produits_ttc = total_produits_ht
 
         discount = value[0][5]
         if not is_nan(discount):
             taux_discount = float(DISCOUNT[discount])
-            total_discount = round(total_produits_ttc * taux_discount / 100,2)
         else :
             discount = None
 
-        prix_final = round(total_produits_ttc - total_discount + frais_port ,2)
+        prix_final = rounder(total_produits_ttc - total_discount + frais_port,2)
 
         html_content = template.render(
 
@@ -151,7 +202,7 @@ def generate_pdf_from_csv(csv_file, html_template, output_dir):
             ville=value[0][20],
             etat=value[0][21],
             code_postal=value[0][22],
-            pays=value[0][23],
+            pays=pays_commande,
             date_facture=value[0][0],
             num_facture=str(key),
             produits= produits,
@@ -159,23 +210,46 @@ def generate_pdf_from_csv(csv_file, html_template, output_dir):
             discount=discount,
             total_produits_ht=total_produits_ht,
             total_produits_ttc=total_produits_ttc,
-            total_produits_tva=total_produits_tva,
-            taux=taux,
+            total_produits_tva1=total_produits_tva1,
+            total_produits_tva2=total_produits_tva2,
+            taux1=taux1,
+            taux2=taux2,
             taux_discount=taux_discount,
             total_discount=total_discount,
             prix_final = prix_final,
-            pays_label = TAUX[value[0][23]][0] if value[0][23] in TAUX else '',
-            frais_port = frais_port
+            pays_label = TAUX[pays_commande][0] if pays_commande in TAUX else '',
+            frais_port = frais_port,
+            frais_port_final = frais_port_final
         )
 
         output_pdf_path = os.path.join(output_dir, f"facture_{key}.pdf")
         HTML(string=html_content).write_pdf(output_pdf_path)
         total_factures_generees +=1
+        print('')
+        print(f' FIN COMMANDE {key}')
 
+    print('')
     print('Total factures générées : ')
     print(total_factures_generees)
     print('---------------------------')
     print('')
+
+
+
+'''
+Préparation de l'environnement + recupération template
+'''
+def get_template(html_template, output_dir):
+    # Chargement du template HTML
+    env = Environment(loader=FileSystemLoader('.'),
+                      comment_start_string='{=',
+                      comment_end_string='=}',
+                      )
+    template = env.get_template(html_template)
+    # Création du répertoire de sortie s'il n'existe pas déjà
+    os.makedirs(output_dir, exist_ok=True)
+    return template
+
 
 date_debut = datetime.datetime.now()
 
@@ -194,9 +268,6 @@ print('Temps de calcul : ')
 print(date_fin-date_debut)
 print()
 print('Penser à vérifer les données dans les factures')
-
-print(rounder(0.012,2))
-print(ceil(0.012))
 
 #    date_vente = value[0][0],
 #    nom_produit = value[0][1],
